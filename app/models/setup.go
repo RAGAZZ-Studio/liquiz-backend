095b9d5513523ab11f74176a1a506adddefbf90d
00shiki <ahmadluhurp@gmail.com>
2022-04-10 07:33:26 +0700

    use DBURL instead to connect to database

--- app/models/setup.go
+++ app/models/setup.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"fmt"
+
 	// "log"
 	"os"
 
@@ -19,14 +19,16 @@ func ConnectDatabase() *gorm.DB {
 	// 	log.Fatalf("Error loading .env file")
 	// }
 
-	DbHost := os.Getenv("DB_HOST")
-	DbUser := os.Getenv("DB_USERNAME")
-	DbPassword := os.Getenv("DB_PASSWORD")
-	DbName := os.Getenv("DB_NAME")
-	DbPort := os.Getenv("DB_PORT")
+	// DbHost := os.Getenv("DB_HOST")
+	// DbUser := os.Getenv("DB_USERNAME")
+	// DbPassword := os.Getenv("DB_PASSWORD")
+	// DbName := os.Getenv("DB_NAME")
+	// DbPort := os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", DbHost, DbUser, DbPassword, DbName, DbPort)
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DBURL := os.Getenv("DATABASE_URL")
+
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", DbHost, DbUser, DbPassword, DbName, DbPort)
+	DB, err := gorm.Open(postgres.Open(DBURL), &gorm.Config{})
 
 	if err != nil && DB == nil {
 		panic("Failed to connect to database!")
