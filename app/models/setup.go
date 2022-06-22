a97259c8fab87975a385e1b770d98f2df4f97dc4
Ahmad Luhur Pakerti <ahmadluhurp@gmail.com>
2022-06-23 05:49:23 +0700

    database for heroku

--- app/models/setup.go
+++ app/models/setup.go
@@ -2,10 +2,10 @@ package models
 
 import (
 	"fmt"
-	"log"
+	// "log"
 	"os"
 
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -13,24 +13,24 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	err := godotenv.Load(".env")
+	// err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 
-	DbHost := os.Getenv("DB_HOST")
-	DbUser := os.Getenv("DB_USERNAME")
-	DbPassword := os.Getenv("DB_PASSWORD")
-	DbName := os.Getenv("DB_NAME")
-	DbPort := os.Getenv("DB_PORT")
+	// DbHost := os.Getenv("DB_HOST")
+	// DbUser := os.Getenv("DB_USERNAME")
+	// DbPassword := os.Getenv("DB_PASSWORD")
+	// DbName := os.Getenv("DB_NAME")
+	// DbPort := os.Getenv("DB_PORT")
 
-	// DBURL := os.Getenv("DATABASE_URL")
+	DBURL := os.Getenv("DATABASE_URL")
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", DbHost, DbUser, DbPassword, DbName, DbPort)
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	// DB, err := gorm.Open(postgres.Open(DBURL), &gorm.Config{})
+	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err := gorm.Open(postgres.Open(DBURL), &gorm.Config{})
 
 	if err != nil && DB == nil {
 		panic("Failed to connect to database!")
